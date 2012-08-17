4cc6a4b05146ebd7f0abe154f646ad66c2829bac
Sam Critchley <critchley.sj@gmail.com>
2012-08-18 00:15:47 +1000

    automating worknik api key in msbuild

--- PasswordTool.Services/IWordService.cs
+++ PasswordTool.Services/IWordService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
@@ -20,10 +19,12 @@ namespace PasswordTool.Services
     {
         private readonly Uri ServiceUri = new Uri("https://api.wordnik.com//v4/words.json/");
 
+        #region IWordService Members
+
         public Task<IEnumerable<WordItem>> RandomWords(int? minimumWordLength, int? maximumResults)
         {
             var client = new HttpClient();
-            var path = "randomWords?hasDictionaryDef=true&minCorpusCount=1000";
+            string path = "randomWords?hasDictionaryDef=true&minCorpusCount=1000&maxLength=8";
             if (minimumWordLength.HasValue)
                 path = string.Format("{0}&minLength={1}", path, minimumWordLength);
             if (maximumResults.HasValue)
@@ -32,29 +33,30 @@ namespace PasswordTool.Services
             var requestUri = new Uri(ServiceUri, new Uri(path, UriKind.Relative));
             client.DefaultRequestHeaders.Add("api_key", ConfigurationManager.AppSettings["WordnikAPIKey"]);
 
-            var task = client.GetAsync(requestUri);
+            Task<HttpResponseMessage> task = client.GetAsync(requestUri);
             return Task.Factory.StartNew(() =>
-                                      {
-                                          task.Wait(new TimeSpan(0, 0, 30));
-                                          var response = task.Result;
-                                          if (response.IsSuccessStatusCode)
-                                          {
-                                              var contentTask = response.Content.ReadAsStringAsync();
-                                              contentTask.Wait(new TimeSpan(0, 0, 30));
-                                              var content = contentTask.Result;
-                                              return
-                                                  Newtonsoft.Json.JsonConvert.DeserializeObject<WordItem[]>(
-                                                      content,
-                                                      new JsonSerializerSettings()
-                                                          {
-                                                              ContractResolver =
-                                                                  new CamelCasePropertyNamesContractResolver()
-                                                          }).AsEnumerable();
-                                          }
+                                             {
+                                                 task.Wait(new TimeSpan(0, 0, 30));
+                                                 HttpResponseMessage response = task.Result;
+                                                 if (response.IsSuccessStatusCode)
+                                                 {
+                                                     Task<string> contentTask = response.Content.ReadAsStringAsync();
+                                                     contentTask.Wait(new TimeSpan(0, 0, 30));
+                                                     string content = contentTask.Result;
+                                                     return
+                                                         JsonConvert.DeserializeObject<WordItem[]>(
+                                                             content,
+                                                             new JsonSerializerSettings
+                                                                 {
+                                                                     ContractResolver =
+                                                                         new CamelCasePropertyNamesContractResolver()
+                                                                 }).AsEnumerable();
+                                                 }
 
-                                          return null;
-                                      });
-
+                                                 return null;
+                                             });
         }
+
+        #endregion
     }
 }
